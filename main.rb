f0b5813b4d656281af0a740d462bc09ea4bf5d69
Patrick Lu <patlu08@gmail.com>
2012-08-12 10:31:40 -0500

    getting user email 5

--- main.rb
+++ main.rb
@@ -48,7 +48,7 @@ post '/' do
   puts "*"*80
   
   doc = Nokogiri::HTML.parse(body)
-  body = doc.css('div.gmail_quote').text.strip()
+  body = doc.css('div.gmail_quote').strip()
   
   page_object = Page.create({
     :from => from,
