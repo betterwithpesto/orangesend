0542526cfb7bb0f5693535c92a50a351f31a0650
Patrick Lu <patlu08@gmail.com>
2012-08-12 08:29:46 -0500

    body debugging encoding to binary 3

--- main.rb
+++ main.rb
@@ -31,7 +31,7 @@ end
 
 get '/page' do
   o = Page.find(params[:id])
-  "From: #{o.from}, Subject: #{o.subject}, Page: #{o.body.to_s}"
+  "From: #{o.from}, Subject: #{o.subject}, Page: #{o.body.to_html}"
 end
   
   
